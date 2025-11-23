a8e3c1803d34fc3a933c493b552724994d1447ba
Hareesh-Raj <hareesh1705@gmail.com>
2025-11-23 21:45:11 +0530

    Added the recommendation data

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,10 @@ export default {
   darkMode: 'class',
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+        heading: ['Outfit', 'sans-serif'],
+      },
       colors: {
         // Custom colors for glassmorphism and themes
       },
